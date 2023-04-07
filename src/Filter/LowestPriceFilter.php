a5526dad8f8f2fb8c0d59560043bf9f4d08e71d2
Liviu Dragulin <liviu.dragulin@gmail.com>
2023-04-07 13:35:13 +0300

    Implement the Factory Pattern for Price Modifiers based on the Promotions types

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -4,22 +4,31 @@ namespace App\Filter;
 
 use App\DTO\PromotionEnquiryInterface;
 use App\Entity\Promotion;
+use App\Filter\Modifier\Factory\PriceModifierFactoryInterface;
 
 class LowestPriceFilter implements PromotionsFilterInterface
 {
-    public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
+    public function __construct(private PriceModifierFactoryInterface $priceModifierFactory)
+    {
+    }
+
+    public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotions): PromotionEnquiryInterface
     {
         $price = $enquiry->getProduct()->getPrice();
         $quantity = $enquiry->getQuantity();
         //$lowestPrice = $quantity * $price;
 
-        //$modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
+        foreach ($promotions as $promotion) {
+            $priceModifier = $this->priceModifierFactory->create($promotion->getType());
+            
+            $priceModifier->modify($price, $quantity, $promotion, $enquiry);
 
-        //TODO: split this into interfaces to resolve the Interface Segregation Principle
-        $enquiry->setDiscountedPrice(50);
-        $enquiry->setPrice(100);
-        $enquiry->setPromotionId(3);
-        $enquiry->setPromotionName('Christmas sales mega discounts');
+            //TODO: split this into interfaces to resolve the Interface Segregation Principle
+            $enquiry->setDiscountedPrice(50);
+            $enquiry->setPrice(100);
+            $enquiry->setPromotionId(3);
+            $enquiry->setPromotionName('Christmas sales mega discounts');
+        }
 
         return $enquiry;
     }
